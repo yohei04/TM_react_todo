695b9ea2ab83010b205b0e13bc71799b807b6531
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-01 23:14:36 +0900

    create todo.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Dinner with wif',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false,
+      },
+    ]
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Todos extends Component {
+  render() {
+    console.log(this.props.todos)
+    return this.props.todos.map((todo) => (
+      <h3>{ todo.title }</h3>
+    ));
+  }
+}
+
+export default Todos;
