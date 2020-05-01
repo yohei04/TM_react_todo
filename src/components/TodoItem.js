2ad5fccab16777e4e28af319eae68e5812a83c3c
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-02 00:30:26 +0900

    how to use style with method

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' :'none'
+    }
+  }
+
+  markComplete = (e) => {
+    console.log(this.props)
+  }
+
   render() {
     return (
-      <div>
-        <p> { this.props.todo.title } </p>
+      <div style={ this.getStyle() }>
+        <p>
+          <input type="checkbox" onChange={this.markComplete} /> {' '}
+          {this.props.todo.title}
+        </p>
       </div>
     )
   }
@@ -15,6 +31,8 @@ export class TodoItem extends Component {
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
 }
+
+
 
 export default TodoItem
 
